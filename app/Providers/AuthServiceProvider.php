a9bbc754a3b23889065bc39f1661c888ddde0463
Camilo Osorio <brycos15@gmail.com>
2022-12-08 19:57:15 -0500

    Se agrega información personalizada al correo de verificación

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +28,16 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        //Configuración de la información del correo de verificación
+        VerifyEmail::toMailUsing( function($notifiable, $url) {
+            return (new MailMessage)
+                //Asunto del correo
+                ->subject('Verificar Cuenta JobsApp')
+                //Parrafo del correo
+                ->line('Tu cuenta está casi lista, presiona el siguiente enlace para verificarla.')
+                //Boton de verificación
+                ->action('Confirmar cuenta', $url)
+                ->line('Si no solicitaste crear esta cuenta, puedes ignorar este mensaje');
+        });
     }
 }
